e8d9a102ab68d7dbaeead5ef22d197390d4ebc2d
Adil ABBADI <adil@abbadi.com>
2021-09-25 21:34:15 +0100

    v1.1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from downloads import EsriWorldStreetMap
 
 ersi = EsriWorldStreetMap()
 
-
+# cd /home/adil/dev/python/leaflet/download && venv/bin/python main.py
 
 if __name__ == "__main__":
     ersi.download()
