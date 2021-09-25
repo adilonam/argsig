6f02e47c8cdecffa99978e969c3d3d709a94c13c
Adil ABBADI <adil@abbadi.com>
2021-09-25 22:02:45 +0100

    print in same line

--- downloads.py
+++ downloads.py
@@ -1,16 +1,20 @@
 import requests
 from pathlib import Path
 
+
+
+BIG_NUMBER = int(10e100)
+
 class EsriWorldStreetMap():
     url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}/'
-    max_z = 10e100
-    max_y= 10e100
-    max_x = 10e100
+    max_z = BIG_NUMBER
+    max_y= BIG_NUMBER
+    max_x = BIG_NUMBER
     def download(self, path = "./download"):
         f_default = open("./assets/default.jpeg", "rb")
         default = f_default.read()
         index = 0
-        count = self.max_z *self.max_y* self.max_x
+        _index = 1
         break_y = False
         break_z = False
         print("Begin")
@@ -22,11 +26,12 @@ class EsriWorldStreetMap():
                     break_y = False
                     break
                 for x in range(self.max_x):
-                    index += 1
-                    if index % 50 :
-                        print("Progress = {:.2f}%".format(index*100/count))
+                    if index % 20 and index != _index :
+                        _index = index
+                        print("{} images téléchargées".format(index),end="\r")
                     r = requests.get(self.url.format(z=z,y=y,x=x))
                     if (default != r.content):
+                        index += 1
                         _path = '{path}/{z}/{y}/'.format(path=path,z=z,y=y)
                         Path(_path).mkdir(parents=True, exist_ok=True)
                         with open("{}/{}.jpeg".format(_path, x), 'wb') as f:
