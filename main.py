e4ea5eeb1892eef35d28c054f52df83b78437450
Adil ABBADI <adil.abbadi@ifazs.gov.ma>
2021-12-29 20:08:43 +0100

    param on main

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ ersi = EsriWorldStreetMap()
 
 if __name__ == "__main__":
     # ersi.download()
-    test = ersi.specific()
+    test = ersi.specific( begin = (10.756043395476832, 15.151256455158718), end = (47.10696511636121, -33.57353845655261))
 
 
